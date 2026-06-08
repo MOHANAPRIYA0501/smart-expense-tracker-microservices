05d0d862ece9e3a9ed1057e0cf3df83e273bc8a6
MOHANAPRIYA0501 <mohanapriyachokkanathan@gmail.com>
2026-06-08 22:46:30 +0530

    Added Eureka service registry

---
+++ service-registry/src/test/java/com/mona/expensetracker/registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mona.expensetracker.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
