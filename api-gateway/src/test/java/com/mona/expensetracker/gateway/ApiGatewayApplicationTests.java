174cbf5a5d6a143f36a5cd57cae980e70eec9d80
MOHANAPRIYA0501 <mohanapriyachokkanathan@gmail.com>
2026-06-08 23:37:19 +0530

    Added API Gateway and Eureka integration

---
+++ api-gateway/src/test/java/com/mona/expensetracker/gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mona.expensetracker.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
